0be8984f05817e1e5f938aab72b1e544b18f18f5
BogdanTop <lbi2001@yandex.ru>
2019-07-28 16:54:11 +0300

    desktop

---
+++ AKASH/Assets/Scripts/Bogdan/PC_Folder/PC_Script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PC_Script : MonoBehaviour, IBeginDragHandler,IDragHandler,IEndDragHandler
+{
+
+    private Vector3 startPosition;
+    private Vector3 offset;
+    private float minDist = 30;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startPosition = transform.position;
+        offset = Input.mousePosition - startPosition;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition - offset;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (Vector3.Distance(startPosition, transform.position) < minDist) 
+        {
+            transform.position = startPosition;
+        }
+    }
+}
